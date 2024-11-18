a049affbb5db2d88884561c09dc5167e6302dcc0
Matt Lacey <matt@mrlacey.co.uk>
2024-11-18 12:10:11 +0000

    update project dependencies and conventions

---
+++ src/XamlStyleTypes/OutputPane.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace XamlStyleTypes
+{
+	public class OutputPane
+	{
+		private static Guid xstPaneGuid = new Guid("14EF4121-68BF-4BC7-AE29-D95FA115241F");
+
+		private static OutputPane instance;
+
+		private readonly IVsOutputWindowPane pane;
+
+		private OutputPane()
+		{
+			ThreadHelper.ThrowIfNotOnUIThread();
+
+			if (ServiceProvider.GlobalProvider.GetService(typeof(SVsOutputWindow)) is IVsOutputWindow outWindow
+			 && (ErrorHandler.Failed(outWindow.GetPane(ref xstPaneGuid, out pane)) || pane == null))
+			{
+				if (ErrorHandler.Failed(outWindow.CreatePane(ref xstPaneGuid, Vsix.Name, 1, 0)))
+				{
+					System.Diagnostics.Debug.WriteLine("Failed to create the Output window pane.");
+					return;
+				}
+
+				if (ErrorHandler.Failed(outWindow.GetPane(ref xstPaneGuid, out pane)) || (pane == null))
+				{
+					System.Diagnostics.Debug.WriteLine("Failed to get access to the Output window pane.");
+				}
+			}
+		}
+
+		public static OutputPane Instance => instance ??= new OutputPane();
+
+		public async Task ActivateAsync()
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+			pane?.Activate();
+		}
+
+		public async Task WriteAsync(string message)
+		{
+			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(CancellationToken.None);
+
+			_ = (pane?.OutputStringThreadSafe($"{message}{Environment.NewLine}"));
+		}
+	}
+}
--- src/XamlStyleTypes/VSPackage.cs
+++ src/XamlStyleTypes/VSPackage.cs
@@ -1,31 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Community.VisualStudio.Toolkit;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TextTemplating.VSHost;
 
 namespace XamlStyleTypes
 {
-    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]   
-    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
-    [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(PackageGuids.XamlStyleTypesString)]
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[Guid(PackageGuids.XamlStyleTypesString)]
 
-    [ProvideCodeGenerator(typeof(MauiStyleGenerator), MauiStyleGenerator.Name, MauiStyleGenerator.Description, true, RegisterCodeBase = true)]
-    [ProvideCodeGeneratorExtension(MauiStyleGenerator.Name, ".xaml")]
+	[ProvideCodeGenerator(typeof(MauiStyleGenerator), MauiStyleGenerator.Name, MauiStyleGenerator.Description, true, RegisterCodeBase = true)]
+	[ProvideCodeGeneratorExtension(MauiStyleGenerator.Name, ".xaml")]
 
-    [ProvideUIContextRule(PackageGuids.CommandVisisiblityString,
-     name: "XAML files",
-     expression: "IsXaml",
-     termNames: new[] { "IsXaml" },
-     termValues: new[] { "HierSingleSelectionName:.xaml$" })]
-    public sealed class VSPackage : ToolkitPackage
-    {
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        {
-            await this.RegisterCommandsAsync();
-        }
-    }
+	[ProvideUIContextRule(PackageGuids.CommandVisisiblityString,
+	 name: "XAML files",
+	 expression: "IsXaml",
+	 termNames: new[] { "IsXaml" },
+	 termValues: new[] { "HierSingleSelectionName:.xaml$" })]
+	public sealed class VSPackage : ToolkitPackage
+	{
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await this.RegisterCommandsAsync();
+
+			await TrackBasicUsageAnalyticsAsync();
+		}
+
+		private static async Task TrackBasicUsageAnalyticsAsync()
+		{
+			try
+			{
+#if !DEBUG
+				if (string.IsNullOrWhiteSpace(AnalyticsConfig.TelemetryConnectionString))
+				{
+					return;
+				}
+
+				var config = new TelemetryConfiguration
+				{
+					ConnectionString = AnalyticsConfig.TelemetryConnectionString,
+				};
+
+				var client = new TelemetryClient(config);
+
+				var properties = new Dictionary<string, string>
+				{
+					{ "VsixVersion", Vsix.Version },
+					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+				};
+
+				client.TrackEvent(Vsix.Name, properties);
+#endif
+			}
+			catch (Exception exc)
+			{
+				System.Diagnostics.Debug.WriteLine(exc);
+				await OutputPane.Instance.WriteAsync("Error tracking usage analytics: " + exc.Message);
+			}
+		}
+	}
 }
