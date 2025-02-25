61736f1d41a01e3ee59b7959ae1e8ca6b535bcba
Matt Lacey <matt@mrlacey.co.uk>
2025-02-25 11:20:46 +0000

    Update AnalyticsConfig.cs

--- src/XamlStyleTypes/AnalyticsConfig.cs
+++ src/XamlStyleTypes/AnalyticsConfig.cs
@@ -2,6 +2,6 @@
 {
 	public static class AnalyticsConfig
 	{
-		public static string TelemetryConnectionString { get; set; } = "InstrumentationKey=b27da4fc-3b77-4cfd-91fc-7f1a57dec160;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=fded251b-82c7-4e63-985b-7618186026be";
+		public static string TelemetryConnectionString { get; set; } = "";
 	}
 }
